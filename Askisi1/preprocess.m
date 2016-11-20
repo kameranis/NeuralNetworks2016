e231d2824b95f3d180a78be56a64c2e03457bf9a
kameranis <kameranis@gmail.com>
2016-11-20 15:34:04 +0200

    Break up code into functions
    Currently can't print neurons list properly

---
+++ Askisi1/preprocess.m
@@ -0,0 +1,33 @@
+function [TrainData, TrainDataTargets, TestData, TestDataTargets] = preprocess(TrainData, TrainDataTargets, TestData, TestDataTargets)
+%PREPROCESS Summary of this function goes here
+%   Detailed explanation goes here
+% Keep the same amount of instances for every label
+examples_per_label = min(sum(TrainDataTargets, 2));
+fprintf('Keeping %d examples for each label\n', examples_per_label);
+
+index_1 = find(TrainDataTargets(1,:), examples_per_label);
+index_2 = find(TrainDataTargets(2,:), examples_per_label);
+index_3 = find(TrainDataTargets(3,:), examples_per_label);
+index_4 = find(TrainDataTargets(4,:), examples_per_label);
+index_5 = find(TrainDataTargets(5,:), examples_per_label);
+
+indexes = [index_1 index_2 index_3 index_4 index_5];
+permutation=randperm(size(indexes,2));
+indexes=indexes (permutation);
+TrainData=TrainData(:,indexes);
+TrainDataTargets=TrainDataTargets(:,indexes) ;
+
+% Remove constant rows
+[TrainData, PS] = removeconstantrows(TrainData);
+TestData = removeconstantrows('apply', TestData, PS);
+
+% Normalize Data
+[TrainData, PS] = mapstd(TrainData);
+TestData = mapstd('apply', TestData, PS);
+
+% Implement PCA
+[TrainData, PS] = processpca(TrainData, 0.002);
+TestData = processpca('apply', TestData, PS);
+
+end
+
---
+++ Askisi1/testNN.m
@@ -0,0 +1,22 @@
+function [ accuracy ] = testNN(TrainData, TrainDataTargets, TestData, TestDataTargets, neurons )
+%TESTNN Summary of this function goes here
+%   Detailed explanation goes here
+net = newff(TrainData, TrainDataTargets, neurons);
+
+% Configure the Neural Network
+net.divideParam.trainRatio = 0.8;
+net.divideParam.valRatio = 0.2;
+net.divideParam.testRatio = 0;
+
+net.trainParam.epochs = 1000;
+
+% Train the Neural Network
+net = train(net, TrainData, TrainDataTargets);
+
+% Get output
+TestDataOutput = sim(net, TestData);
+[accuracy,precision,recall] = eval_Accuracy_Precision_Recall(TestDataOutput,TestDataTargets);
+fprintf('Hidden Layer: %d, Neurons: %2.0f, Accuracy: %f\n', size(neurons, 2), neurons, accuracy);
+
+end
+
