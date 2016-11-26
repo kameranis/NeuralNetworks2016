3fd728a62e99898c64ff1fef073beac6e719f13d
Vasileios Gkoumas <bgoumas@gmail.com>
2016-11-26 12:41:46 +0200

    Metric function

---
+++ Askisi1/metric.m
@@ -0,0 +1,13 @@
+function [ score ] = metric(values , options)
+%Placeholder for more complex metric functions
+
+    switch options
+        case 'fscore'
+            precision = mean(values.precision);
+            recall = mean(values.recall);
+            score = 2*precision*recall/(precision+recall);
+        case 'accuracy'
+            score = accuracy;
+            
+    end
+end
