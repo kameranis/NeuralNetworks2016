eb96bcddc7c018be4a34d8a958ab0dbe529fbf10
kameranis <kameranis@gmail.com>
2017-01-08 14:22:45 +0200

    Refactor main into simple functions

--- Askisi2/main.m
+++ Askisi2/main.m
@@ -1,16 +1,7 @@
 %% Self Organising Maps - SOMs
 close all; clear; clc;
 
-% Load simple data
-EightData;
-QuestionData;
-Cities;
-GroupData;
-
-datasets = {EightPatterns, QuestionPatterns, CityCoordinates, GroupPatterns};
-dataset_names = {'Eight', 'Question', 'Cities', 'Group'};
-
-global IW distances;
+[datasets, dataset_names] = loadDatasets();
 
 % Set SOM parameters
 gridSizes = [[1 5]; [1 10]; [1 15]; [1 20]; [1 30]; [5 5]; [5 10]; [10 5]; [10 10]]';
@@ -18,56 +9,15 @@ orderLR = 0.9;
 orderEpochs = 250;
 tuneLR = 0.1;
 
-% Create SOM for different topologies and distance functions
-for topology={@gridtop, @hexagonalTopology}
-    for distFunction={@dist, @mandist};
-        somTrainParameters(orderLR, orderEpochs, tuneLR, topology{1}, distFunction{1});
-        
-        % Eight Patterns
-        gridSize = [10 5];
-        mM = minmax(datasets{1});
-        somCreate(mM, gridSize);
-        somTrain(datasets{1});
-        figure;
-        plot2DSomData(IW, distances, datasets{1})
+% 2A2
+runTopologies(datasets, orderLR, orderEpochs, tuneLR);
 
-        % Question Patterns
-        gridSize = [1 20];
-        mM = minmax(datasets{2});
-        somCreate(mM, gridSize);
-        somTrain(datasets{2});
-        figure;
-        plot2DSomData(IW, distances, datasets{2})
-    end
-end
+% 2A3
+runGridSizes(datasets, orderLR, orderEpochs, tuneLR);
 
-% Create SOM for different grid sizes for gridtop and dist
-somTrainParameters(orderLR, orderEpochs, tuneLR, @gridtop, @dist);
+% 2B
+runTSP(datasets{3}, orderLR, orderEpochs, tuneLR);
 
-for i=1:2
-    for gridSize=gridSizes
-        mM = minmax(datasets{i});
-        somCreate(mM, gridSize');
-        somTrain(datasets{i});
-        figure;
-        plot2DSomData(IW, distances, datasets{i});
-    end
-end
+% 2C
+runClassification(datasets{4}, orderLR, orderEpochs, tuneLR);
 
-% Solve TSP using SOM
-somTrainParameters(orderLR, orderEpochs, tuneLR, @gridtop, @ring_dist);
-
-mM = minmax(CityCoordinates);
-somCreate(mM, [1 60]);
-somTrain(CityCoordinates);
-figure;
-plot2DSomData(IW, distances, CityCoordinates);
-
-% Classification using SOM
-somTrainParameters(orderLR, orderEpochs, tuneLR, @hexagonalTopology, @dist);
-
-mM = minmax(GroupPatterns);
-somCreate(mM, [5 5]);
-somTrain(GroupPatterns);
-figure;
-somShow(IW, [5 5]);
